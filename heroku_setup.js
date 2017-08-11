24e70ce10889cca36c9b9a88b64ff3a8c6420205
Jacob Stoebel <jstoebel@gmail.com>
2017-08-11 11:27:07 -0400

    allow console.log for entire file

--- heroku_setup.js
+++ heroku_setup.js
@@ -1,69 +1,70 @@
-const commandLineArgs = require('command-line-args')
+/* eslint-disable no-alert, no-console */
+const commandLineArgs = require('command-line-args');
 const exec = require('child-process-promise').exec;
-const thisPackage = require('./package.json')
+const thisPackage = require('./package.json');
 const chalk = require('chalk');
-const emoji = require('node-emoji')
-const randomBytes = require('crypto').randomBytes
+const emoji = require('node-emoji');
+const randomBytes = require('crypto').randomBytes;
 
 
 const optionDefinitions = [
-        { name: 'name', alias: 'n', type: String, defaultValue: thisPackage.name},
+        {name: 'name', alias: 'n', type: String, defaultValue: thisPackage.name},
     ];
 
-const options = commandLineArgs(optionDefinitions)
+const options = commandLineArgs(optionDefinitions);
 
-const reportSuccess = function (result) {
+const reportSuccess = function(result) {
     // prints results from stdout and stderr
-    
-    const stdout = result.stdout
-    console.log(`${emoji.get('white_check_mark')} ${chalk.green(stdout)}`)
-}
+
+    const stdout = result.stdout;
+    console.log(`${emoji.get('white_check_mark')} ${chalk.green(stdout)}`);
+};
 
 const reportError = function(err) {
-    console.log(`${emoji.get('heavy_exclamation_mark')} ${chalk.red.bold(err)}`)
-}
+    console.log(`${emoji.get('heavy_exclamation_mark')} ${chalk.red.bold(err)}`);
+};
 
-console.log(`Creating your heroku instance for ${options.name}...`)
+console.log(`Creating your heroku instance for ${options.name}...`);
 exec(`heroku apps:create ${options.name}`)
     .then(function(result) {
         // created new app!
-        reportSuccess(result)
-    }).then(function(){
-        // next spin up MongoLab 
-        console.log("creating MongoLab instance...")
+        reportSuccess(result);
+    }).then(function() {
+        // next spin up MongoLab
+        console.log('creating MongoLab instance...');
         exec('heroku addons:create mongolab')
             .then(function(result) {
                 // created MongoLab instance
-                reportSuccess(result)
+                reportSuccess(result);
             })
-            .then(function(){
+            .then(function() {
                 // next add app secret
                 randomBytes(48, function(err, buffer) {
                   const token = buffer.toString('hex');
-                  console.log("adding app secret...")
+                  console.log('adding app secret...');
                   exec(`heroku config:set SESSION_SECRET=${token}`)
-                    .then(function(result){
+                    .then(function(result) {
                         // added session secret
-                        reportSuccess(result)
+                        reportSuccess(result);
                     })
-                    .then(function(){
+                    .then(function() {
                         // finished successfully!
-                        console.log(`${emoji.get('rocket')} ${chalk.green.bold(`${options.name} created successfully!`)}`)
-                    }).catch(function(err){
+                        console.log(`${emoji.get('rocket')} ${chalk.green.bold(`${options.name} created successfully!`)}`);
+                    }).catch(function(err) {
                         // failed to set session secret
-                        reportError(err)
-                        process.exit()
-                    })
+                        reportError(err);
+                        process.exit();
+                    });
                 });
             })
-            .catch(function(err){
+            .catch(function(err) {
                 // failed to create MongoLab instance
-                reportError(err)
-                process.exit()
-            })
+                reportError(err);
+                process.exit();
+            });
     })
-    .catch(function(err){
+    .catch(function(err) {
         // failed to create new app
-        reportError(err)
-        process.exit()
-    })
+        reportError(err);
+        process.exit();
+    });
